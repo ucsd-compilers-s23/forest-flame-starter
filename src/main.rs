9cea549206988ac5819a6f4feb38a5e944bf9b35
Nico Lehmann <nico.lehmannm@gmail.com>
2023-05-15 11:32:07 -0700

    Add syntax and parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env,
+    fs::File,
+    io::{self, Read},
+};
+
+mod parser;
+mod syntax;
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let in_name = &args[1];
+    let out_name = &args[2];
+    let mut in_contents = String::new();
+    let mut in_file = File::open(in_name)?;
+    in_file.read_to_string(&mut in_contents)?;
+    let expr = parser::parse(&in_contents);
+    println!("{expr:#?}");
+    // let result = compile(&expr);
+
+    Ok(())
 }
